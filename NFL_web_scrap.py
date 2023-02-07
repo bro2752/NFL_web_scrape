5808a39bdb947a6f7bc365b9cc7063a6e09a2a4b
Brandon Robert Owens <bro@Brandons-MacBook-Air.local>
2023-02-07 17:10:52 +0000

    got players name

--- NFL_web_scrap.py
+++ NFL_web_scrap.py
@@ -1,5 +1,19 @@
 import requests 
-from bs4 import BeautifulSoup4
+from bs4 import BeautifulSoup
 #1. Make Git repo
 #2. Create a virtual environment 
 #3. Install Dependencies 
+
+url = "https://www.nfl.com/stats/player-stats/"
+
+#make requests to website
+html_text = requests.get(url).text
+soup = BeautifulSoup(html_text, "lxml")
+
+table_selector = "d3-o-table d3-o-table--detailed d3-o-player-stats--detailed d3-o-table--sortable"
+player_div = soup.find("table", class_= table_selector)
+
+player_selector = "d3-o-player-fullname nfl-o-cta--link"
+name = soup.find("a", class_= player_selector)
+
+print(name)
